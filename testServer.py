493919742023732a8da7e464163f320e747a3531
Young <chungsan@hotmail.com>
2019-09-16 16:51:01 -0700

    First flask file for test

---
+++ testServer.py
@@ -0,0 +1,14 @@
+from werkzeug.wrappers import Request, Response
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World! <br>This is Young"
+
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple('localhost', 9000, app)
